cf81a4d2e6ef05d73af7b9b286006598139c6660
Danielson Alencar <danielson150@gmail.com>
2019-09-13 15:27:44 -0300

    Add files via upload

---
+++ api_bitcoin.py
@@ -0,0 +1,15 @@
+import requests
+
+print('Informa o valor do bitcoin (Atualizado em 13/09/2019)') 
+
+#json_original = https://api.coindesk.com/v1/bpi/currentprice.json
+#json_modificada = http://www.json-generator.com/api/json/get/bTwonkmGeW?indent=2
+
+url = 'http://www.json-generator.com/api/json/get/bUrNHrYVua?indent=2'
+
+response = requests.get(url)
+json = response.json()
+
+valor = json ['bpi']['BR']['rate']
+
+print('O valor atual do bitcoin é: R$' + valor)
