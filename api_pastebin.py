f7a37098f98bd37da4e6ec927fb47c992010959c
Danielson Alencar <danielson150@gmail.com>
2019-09-13 14:20:24 -0300

    Add files via upload

--- api_pastebin.py
+++ api_pastebin.py
@@ -4,20 +4,18 @@ import requests
 API_ENDPOINT = "http://pastebin.com/api/api_post.php"
 
 # Chave de API gerada na sua conta do pastebin
-API_KEY = "e7592e6e83d2324bb9b7065df9243976"
+#API_KEY = "e7592e6e83d2324bb9b7065df9243976"
+
+API_KEY = input("Digite o seu token:")
 
 # código em python a ser inserido 
-source_code = ''' 
-print("Testando a api do pastebin") 
-a = 10 * 2
-print(a) 
-'''
+source_code = input("Digite o texto a ser inserido:")
 
 # dados a serem enviados para api 
 dados = {'api_dev_key':API_KEY, 
 		'api_option':'paste', 
 		'api_paste_code':source_code, 
-		'api_paste_format':'python'} 
+		'api_paste_format':'gettext'} 
 
 # envia solicitação de postagem e salva a resposta 
 r = requests.post(url = API_ENDPOINT, data = dados) 
